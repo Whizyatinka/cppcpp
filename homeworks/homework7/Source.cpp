b7652c637cf7a786ba4c39865fc36ee224da28eb
Mikhail Makhteev <131249741+Whizyatinka@users.noreply.github.com>
2025-12-24 23:30:46 +0300

    Update Source.cpp

--- homeworks/homework7/Source.cpp
+++ homeworks/homework7/Source.cpp
@@ -1,12 +1,10 @@
 #include <Python.h>
 #include <ctime>
 
-// C-ôóíêöèÿ: ñóììèðîâàíèå ýëåìåíòîâ ñïèñêà + çàìåð âðåìåíè
 static PyObject* addList_add(PyObject* self, PyObject* args)
 {
     PyObject* listObj;
 
-    // Îæèäàåì îäèí àðãóìåíò: ñïèñîê
     if (!PyArg_ParseTuple(args, "O", &listObj))
         return NULL;
 
@@ -14,39 +12,38 @@ static PyObject* addList_add(PyObject* self, PyObject* args)
 
     clock_t start_time = clock();
 
-    long sum = 0;  // ñóììà êàê long, ÷òîáû íå ïåðåïîëíÿëñÿ int
+    long sum = 0; 
 
     for (Py_ssize_t i = 0; i < length; i++) {
         PyObject* temp = PyList_GetItem(listObj, i);
-        long elem = PyLong_AsLong(temp);  // ñ÷èòàåì, ÷òî ýëåìåíòû — int
+        long elem = PyLong_AsLong(temp);  
         sum += elem;
     }
 
     clock_t end_time = clock();
     double time_taken = double(end_time - start_time) / CLOCKS_PER_SEC;
 
-    // Âîçâðàùàåì êîðòåæ (ñóììà, âðåìÿ)
+
     return Py_BuildValue("(ld)", sum, time_taken);
 }
 
-// Òàáëèöà ôóíêöèé ìîäóëÿ
 static PyMethodDef addList_funcs[] = {
     {"add", (PyCFunction)addList_add, METH_VARARGS,
      "add(list) -> (sum, time)"},
     {NULL, NULL, 0, NULL}
 };
 
-// Îïèñàíèå ìîäóëÿ
+
 static struct PyModuleDef addList_module = {
     PyModuleDef_HEAD_INIT,
-    "addList",          // èìÿ ìîäóëÿ (import addList)
+    "addList",          
     "Add all the lists",
     -1,
     addList_funcs
 };
 
-// Èíèöèàëèçàöèÿ ìîäóëÿ
 PyMODINIT_FUNC PyInit_addList(void)
 {
     return PyModule_Create(&addList_module);
 }
+
